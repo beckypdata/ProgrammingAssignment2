85996b22df7d9dec035830e3eaa25216a33e90c5
Becky Portier <beckyp.class@gmail.com>
2015-01-17 11:53:47 -0500

    Initial commit of cachiing inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(xinv) m <<- xinv
+  getinverse <- function() m
+  list(set = set, get = get, 
+       setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +21,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
+
+z <- matrix(sample(1:100, 16), 4, 4)
+print(z)
+w <- makeCacheMatrix(z)
+print(w)
+zinv <- cacheSolve(w)
+print(zinv)
+zinv <- cacheSolve(w)
+print(zinv)
+print(z%*%zinv)
